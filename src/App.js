34dd20521e4d2ec14d5363ec172e7c82f636191d
rattawut jumprom <gotratta@gmail.com>
2020-06-25 13:22:28 +0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import logo from "./assets/images/logo.png";
 
 const App = () => {
-  return <div className="app">Good Luck</div>;
+  return (
+    <div className="app">
+      <img id="logo" alt="logo" src={logo} />
+      <div className="container">
+        <div className="text-center">
+          <span className="title text-center">ชื่อของคุณ</span>
+        </div>
+        <div className="text-center">
+          <input className="input" type="text"></input>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
